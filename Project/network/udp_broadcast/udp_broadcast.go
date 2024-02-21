2ec75c0057fad556ec358bc934dc7a917fffb956
Antonia Helene Aanesen <antoniaa@stud.ntnu.no>
2024-02-21 16:31:54 +0100

    La til ferdig cost function, må finne ut hvordan den brukes og begynt med udp broadcast

--- Project/network/udp_broadcast/udp_broadcast.go
+++ Project/network/udp_broadcast/udp_broadcast.go
@@ -1,1 +1,22 @@
 package udp_broadcast
+
+import (
+	"log"
+	"net"
+)
+
+const broadcastAddress = "255.255.255.255"
+const broadcasrPort = "20006"
+
+func GetLocalIP() net.IP {
+    conn, err := net.Dial("udp", "8.8.8.8:80")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer conn.Close()
+
+    localAddress := conn.LocalAddr().(*net.UDPAddr)
+
+    return localAddress.IP
+}
+
